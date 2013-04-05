878222b17f67be331cc42f05a78b843441aae708
Adam Gerber <adam.gerber@yahoo.com>
2013-04-05 11:32:04 -0500

    p0

--- src/edu/uchicago/cs/bthelloworld01/MainActivity.java
+++ src/edu/uchicago/cs/bthelloworld01/MainActivity.java
@@ -1,46 +1,17 @@
 package edu.uchicago.cs.bthelloworld01;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity  {
 
-	// step 1: declare variables
-	EditText edtMessage;
-	Button btnShow;
-
+	// %%%%%%%%%%%%%% PHASE 0
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		// step 2: get references to objects
-		edtMessage = (EditText) findViewById(R.id.edtMessage);
-		btnShow = (Button) findViewById(R.id.btnShow);
-
-		// step 3: register event-listeners
-		btnShow.setOnClickListener(this);
-		// step 4: let enclosing activity implement listener
-		// step 5: add unimplemented methods
 	}
 
-	@Override
-	public void onClick(View v) {
-		// step 6: program behavior
-		String strMessage = edtMessage.getText().toString();
-		Toast.makeText(getApplicationContext(),
-				"This is your message: " + strMessage, Toast.LENGTH_SHORT)
-				.show();
 
-	}
 
 }
