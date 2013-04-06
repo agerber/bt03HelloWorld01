868319a481661833607f525f61f8ace99b79e48c
Adam Gerber <adam.gerber@yahoo.com>
2013-04-05 22:45:33 -0500

    again first

---
+++ src/edu/uchicago/cs/bthelloworld01/HelloThere.java
@@ -0,0 +1,17 @@
+package edu.uchicago.cs.bthelloworld01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloThere extends Activity  {
+
+	// %%%%%%%%%%%%%% PHASE 0
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+
+
+}
